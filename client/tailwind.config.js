8fa5aa938f4fc6880ed94de2c119c30e39c5bf6a
Ankush2107 <ankush.kumar210701@gmail.com>
2024-03-03 17:48:55 +0530

    added some data and images for the project

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#0067ff",
+        yellowColor: "#feb60d",
+        purpleColor: "#9771ff",
+        irisBlueColor: "#01b5c5",
+        headingColor: "#181a1e",
+        textColor: "#4e545f"
+      },
+      boxShadow: {
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px",
+      }
+    },
   },
   plugins: [],
 }
